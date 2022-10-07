c8344b04df9065fcd8f99e5aeefc36c5cbd44f05
Gabizz <113138960+dizzgabizzz@users.noreply.github.com>
2022-10-07 08:30:38 -0300

    Add files via upload

---
+++ Login2f/app/src/main/java/com/inf2fm/login2f/MainActivity.java
@@ -0,0 +1,38 @@
+package com.inf2fm.login2f;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    private void executeNextActivity(){
+
+        startActivity(new Intent(MainActivity.this, Login.class));
+
+
+    }
+
+    public class ClickButton implements View
+
+    .OnClickListener{
+        @Override
+        public void onClick(View view) {
+            executeNextActivity();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.Button1);
+        mButton.setOnClickListener(new ClickButton());
+    }
+}
